266cb053b21cc18389e98c90253e80da98c13f43
Owen Nelson <onelson@gmail.com>
2019-10-07 23:40:42 -0700

    Adding ts types from core to client output.
    In order for this to work nicely, a "typescript" feature was added to
    core which adds `wasm-bindgen` and `typescript-definitions`, which are
    used to annotate the types for the `index.d.ts` generated in `e2e-client`.
    
    Another change made here was to add a rust-toolchain file to the client,
    which now requires nightly to be able to do the ts output magic. Along
    with this, I noticed that I didn't see build failures when running
    `cargo build` directly on the client, but did when running the wasm-pack
    build via npm. This was because wasm-pack switches the target off of
    whatever the default is over to wasm32 instead. To cut down on
    confusion, I configured cargo (in that one crate) to default to wasm32.

--- e2e-core/src/lib.rs
+++ e2e-core/src/lib.rs
@@ -1,12 +1,13 @@
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
-
-// FIXME: add wasm-bindgen and use cfg_attr to conditionally tag each of these
-//  structs. This should avoid the need to make extraneous structs in
-//  `e2e-client` (such as `NewMessage`).
+#[cfg(feature = "typescript")]
+use typescript_definitions::TypescriptDefinition;
+#[cfg(feature = "typescript")]
+use wasm_bindgen::prelude::*;
 
 /// Base fields for a message.
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[cfg_attr(feature = "typescript", derive(TypescriptDefinition))]
 pub struct Message {
     pub author: String,
     pub text: String,
@@ -14,16 +15,20 @@ pub struct Message {
 
 /// Just a message with a timestamp attached, used for output only.
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[cfg_attr(feature = "typescript", derive(TypescriptDefinition))]
 pub struct ChatLogEntry {
+    /// When the entry was collected by the server.
+    #[cfg_attr(feature = "typescript", ts(ts_type = "string"))]
     pub timestamp: DateTime<Utc>,
-    #[serde(flatten)]
-    pub message: Message,
+    /// The message itself.
+    pub msg: Message,
 }
 
 /// This is the response from the List handler.
 ///
 /// Messages in the response are _oldest first_.
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[cfg_attr(feature = "typescript", derive(TypescriptDefinition))]
 pub struct MessageListResponse {
     messages: Vec<ChatLogEntry>,
 }
