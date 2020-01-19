4af8ade0bcb160a3f705e2739c0c987bd674e067
Owen Nelson <onelson@gmail.com>
2020-01-18 22:54:39 -0800

    Move codegen stuff into module.

--- e2e-grpc/src/lib.rs
+++ e2e-grpc/src/lib.rs
@@ -1,15 +1,17 @@
-use std::time::SystemTime;
-
 pub extern crate tonic;
 
-tonic::include_proto!("chatroom");
+pub mod chatroom {
+    use std::time::SystemTime;
 
-impl ChatLogEntry {
-    pub fn new(msg: Message) -> Self {
-        let now = SystemTime::now();
-        Self {
-            msg: Some(msg),
-            timestamp: Some(now.into()),
+    tonic::include_proto!("chatroom");
+
+    impl ChatLogEntry {
+        pub fn new(msg: Message) -> Self {
+            let now = SystemTime::now();
+            Self {
+                msg: Some(msg),
+                timestamp: Some(now.into()),
+            }
         }
     }
 }
