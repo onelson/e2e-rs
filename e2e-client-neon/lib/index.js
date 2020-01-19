0673833e1f4f364d8b02f559055cbb392922519b
Owen Nelson <onelson@gmail.com>
2020-01-19 00:42:39 -0800

    Stubbed out a native chat client.
    Did a quick smoke test to see if the client could connect from nodejs
    (it could!)

--- e2e-client-neon/lib/index.js
+++ e2e-client-neon/lib/index.js
@@ -1,3 +1,5 @@
-var addon = require('../native');
+var addon = require("../native");
 
-console.log(addon.hello());
+module.exports = {
+  ChatroomClient: addon.ChatroomClient
+};
--- e2e-client-neon/native/src/lib.rs
+++ e2e-client-neon/native/src/lib.rs
@@ -1,12 +1,48 @@
-#[macro_use]
-extern crate neon;
-
+use e2e_grpc::chatroom::chatroom_client::ChatroomClient;
+use e2e_grpc::tonic::transport::Channel;
 use neon::prelude::*;
+use tokio::runtime::Runtime;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+type Client = ChatroomClient<Channel>;
+
+pub struct ClientWrapper {
+    client: Client,
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
+impl ClientWrapper {
+    pub fn new(url: String) -> Self {
+        let mut rt = Runtime::new().unwrap();
+
+        Self {
+            client: rt.block_on(ChatroomClient::connect(url)).unwrap(),
+        }
+    }
+}
+
+declare_types! {
+    /// JS class wrapping Employee records.
+    pub class JsChatroomClient for ClientWrapper {
+
+        init(mut cx) {
+            let url: String = cx.argument::<JsString>(0)?.value();
+
+            Ok(ClientWrapper::new(url))
+        }
+
+        method getIdentity(mut ctx) {
+            todo!();
+        }
+
+        method getMessages(mut ctx) {
+            todo!();
+        }
+
+        method createMessage(mut ctx) {
+            todo!();
+        }
+    }
+}
+register_module!(mut m, {
+    m.export_class::<JsChatroomClient>("ChatroomClient")?;
+    Ok(())
 });
